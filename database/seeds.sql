02a25311d65ad429ed871c341067a7f590fad4e8
Shamar Orr <sorr55@gmail.com>
2017-11-21 19:38:09 -0500

    heroku

--- database/seeds.sql
+++
@@ -1,6 +0,0 @@
-USE burgers_db;
-
-INSERT INTO burger(burger_name,devoured) VALUES("BBQ Bacon Burger", False);
-INSERT INTO burger(burger_name,devoured) VALUES("Chicken Burger", False);
-INSERT INTO burger(burger_name,devoured) VALUES("Bison Burger", False);
-INSERT INTO burger(burger_name,devoured) VALUES("Angus Burger", False);
--- server.js
+++ server.js
@@ -1,26 +1,33 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var exphbs = require("express-handlebars");
+var path = require("path");
 
+var PORT = process.env.PORT || 3000;
 
 var app = express();
-app.use(express.static(__dirname + "/public"));
 
-app.use(bodyParser.urlencoded({
-	extended: false
-}))
+var db = require("./models");
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
-app.engine('handlebars',exphbs({
-	defaultLayout: 'main'
-}));
 
-app.set('view engine','handlebars');
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-var routes = require("./controllers/routes.js")
-app.use("/",routes);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-var PORT = 3030;
-app.listen(PORT);
-console.log("Running");
+// Import routes and give the server access to them.
+require("./routes/burger-api-routes.js")(app);
+
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
